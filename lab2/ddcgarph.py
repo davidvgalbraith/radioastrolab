90e1b64316bc711c610f643e9319f9129eacc93b
David <david@david-VPCEB26GM.(none)>
2014-03-02 03:19:34 -0500

    Going to sleep on Sunday morning

---
+++ lab2/ddcgarph.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main(argv):
+   a = np.fft.fftfreq(2048)
+   for x in range(1, 7):
+      k = 10 * x
+      imag = np.fromfile("ddc/ddc_imag_bram_" + k.__str__(), dtype=">i4")
+      real = np.fromfile("ddc/ddc_real_bram_" + k.__str__(), dtype=">i4")
+      ddddc = real + 1j * imag
+      plt.plot(a, abs(np.fft.fft(ddddc)))
+   plt.tick_params(axis='both', which='major', labelsize=24)
+   
+   plt.title("Filter response", fontsize=36)
+   plt.xlabel("Input Frequency(Hz)", fontsize=36)
+   plt.ylabel("Filter output (I don't know the units)", fontsize=36)
+   plt.show()
+if __name__ == "__main__":
+   main(sys.argv[1:])
