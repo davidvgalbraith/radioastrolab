b921bce3ecdcea94572c7d8eb5871ec4c6fb29eb
David <david@david-VPCEB26GM.(none)>
2014-03-01 23:33:19 -0500

    11:30 on a Saturday

--- lab2/mixgraph.py
+++ lab2/mixgraph.py
@@ -13,19 +13,28 @@ def main(argv):
    d["3"] = 95
    graphable = data["arr_" + argv[0]]
    ffted = np.fft.fft(graphable)
-   x = np.arange(0, len(graphable), 1)
+   #x = np.arange(len(ffted)) * 6000 / 1023 - 6000 / 2
+   q = np.arange(len(ffted))/6000000.0
+   ffted[len(ffted)/8:7 * len(ffted)/8] = 0
+   #ffted[len(ffted)/8 * 5:len(ffted)/8 * 7] = 0
+   print abs(ffted)
+   inv = np.fft.ifft(ffted)
    xx = np.fft.fftfreq(len(graphable)) 
-   y = np.arange(0, len(ffted), 1)
-   plt.plot(xx, abs(ffted))
+   print inv
+   #plt.plot(xx, abs(ffted))
    #plt.plot(x, graphable)
+   plt.plot(q, inv)
    plt.tick_params(axis='both', which='major', labelsize=24)
    
    #plt.title("Convoluted signal of 1MHz and " + d[argv[0]].__str__() + " MHz", fontsize=36)
    #plt.xlabel("Time instant (sample #)", fontsize=36)
    #plt.ylabel("Signal", fontsize=36)
-   plt.title("Convoluted Power Spectrum of 1 MHz and " + d[argv[0]].__str__() + " MHz", fontsize=36)
-   plt.xlabel("Frequency(MHz)", fontsize=36)
-   plt.ylabel("Power Amplitude Coefficient", fontsize=36)
+   #plt.title("Convoluted Power Spectrum of 1 MHz and " + d[argv[0]].__str__() + " MHz", fontsize=36)
+   #plt.xlabel("Frequency(kHz)", fontsize=36)
+   #plt.ylabel("Power Amplitude Coefficient", fontsize=36)
+   plt.title("IFFT of convolved, filtered signals at 0.95 MHz and 1 MHz", fontsize=36)
+   plt.xlabel("Time (sec)", fontsize=36)
+   plt.ylabel("Signal", fontsize=36)
    plt.show()
 if __name__ == "__main__":
    main(sys.argv[1:])
