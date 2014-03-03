89e8f238701998c77d299066942d508aaefcdb35
David <david@david-VPCEB26GM.(none)>
2014-03-03 14:50:28 -0500

    Git wants to delete all my code

--- lab2/part2graph.py
+++ lab2/part2graph.py
@@ -11,26 +11,19 @@ def main(argv):
    data = np.fromfile("roach/twelve/mix_bram_" + d[argv[0]] + "_sig_5pct_12dbm", dtype=np.int32)
    #data = np.fromfile("roach/mix_bram_" + d[argv[0]] + "_sig_10pct", dtype=">i4")
    #data = np.fromfile("roach/adc_bram_1", dtype=">i4")
-   print len(data)
-   x = np.arange(len(data))
+   x = np.fft.fftfreq(len(data)) * 200 
+   
    ffted = np.fft.fft(data)
-   inv = np.fft.ifft(ffted)
-   xx = np.fft.fftfreq(len(data)) 
-   print inv
-   plt.plot(xx, abs(ffted))
+   
+   plt.plot(x, abs(ffted))
    #plt.plot(x, data)
    #plt.plot(xx, ffted)
    plt.tick_params(axis='both', which='major', labelsize=24)
    
-   #plt.title("Convoluted signal of 1MHz and " + d[argv[0]].__str__() + " MHz", fontsize=36)
-   #plt.xlabel("Time instant (sample #)", fontsize=36)
-   #plt.ylabel("Signal", fontsize=36)
    plt.title("Digitally Convoluted Power Spectrum of 1 MHz and 1.05 MHz", fontsize=36)
    plt.xlabel("Frequency(MHz)", fontsize=36)
    plt.ylabel("Power Amplitude Coefficient", fontsize=36)
-   #plt.title("IFFT of convolved, filtered signals at 0.95 MHz and 1 MHz", fontsize=36)
-   #plt.xlabel("Time (sec)", fontsize=36)
-   #plt.ylabel("Signal", fontsize=36)
+ 
    plt.show()
 if __name__ == "__main__":
    main(sys.argv[1:])
